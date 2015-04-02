f3b8504dfd6aea466b5963cb5a30083b75ca2e86
RobertJMitchell <rjrocks32@gmail.com>
2015-04-02 10:40:16 -0500

    added new identity classes and roles in seeder

---
+++ TodoUser315.Data.Model/Role.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoUser315.Data.Model
+{
+    public class Role : IdentityRole
+    {
+        //available roles for app/users
+        public const string ADMIN = "Admin";
+        public const string GENERAL = "General";
+        public const string ADMIN_GENERAL = "Admin,General";
+    }
+}
---
+++ TodoUser315.Data/Seeder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoUser315.Data.Model;
+
+namespace TodoUser315.Data
+{
+    public static class Seeder
+    {
+        //Seed Users and Mock Data
+        public static void Seed(TodoDbContext db)
+        {
+            // generic Identity code - store the users
+            UserStore<ApplicationUser> userStore = new UserStore<ApplicationUser>(db);
+            UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(userStore);
+            //generic identity - roles
+            RoleStore<Role> roleStore = new RoleStore<Role>(db);
+            RoleManager<Role> roleManager = new RoleManager<Role>(roleStore);
+        }
+    }
+}
